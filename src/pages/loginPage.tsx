b2ffc0a3e4903e9ed24922a2c68e27ccd48ed805
danielbelnuz18 <danielbelnuz18@gmail.com>
2022-12-15 19:59:59 -0500

    update login ui

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -8,38 +8,67 @@ export function LoginPage() {
 
   const login = (event: React.FormEvent) => {
     event.preventDefault()
-    alert('login successful')
+    console.log(email, password)
   }
 
   return (
-    <form onSubmit={login}>
-      <label htmlFor="email">Email</label>
-      <input
-        required
-        type="email"
-        value={email}
-        onChange={(event) =>
-          serCredentials({
-            email: event.target.value,
-            password,
-          })
-        }
-        placeholder="Email..."
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        required
-        type="password"
-        value={password}
-        onChange={(event) =>
-          serCredentials({
-            email,
-            password: event.target.value,
-          })
-        }
-        placeholder="*************"
-      />
-      <button>Login</button>
-    </form>
+    <section className="h-screen">
+      <div className="px-6 h-full text-gray-800">
+        <div className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6">
+          <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
+            <form onSubmit={login}>
+              <div className="mb-6">
+                <input
+                  required
+                  type="email"
+                  className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  placeholder="Email address"
+                  value={email}
+                  onChange={(event) =>
+                    serCredentials({
+                      email: event.target.value,
+                      password,
+                    })
+                  }
+                />
+              </div>
+              <div className="mb-6">
+                <input
+                  required
+                  type="password"
+                  className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(event) =>
+                    serCredentials({
+                      email,
+                      password: event.target.value,
+                    })
+                  }
+                />
+              </div>
+
+              <div className="text-center lg:text-left">
+                <button
+                  type="button"
+                  className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                >
+                  Login
+                </button>
+                <p className="text-sm font-semibold mt-2 pt-1 mb-0">
+                  Don't have an account?
+                  <a
+                    href="#!"
+                    className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
+                  >
+                    Register
+                  </a>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
